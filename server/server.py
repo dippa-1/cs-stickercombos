775609e829c95487b69de82ea8536cb624e0762a
dippa-1 <dominik.helfenstein@gmx.de>
2024-02-09 22:31:45 +0100

    server: begin some methods for combinations

--- server/server.py
+++ server/server.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask
 import json
 
 app = Flask(__name__)
 
-data = None
+all_stickers = None
 with open('data/stickerAll.json', 'r') as f:
-  data = json.load(f)
+  all_stickers = json.load(f)
 
-# Endpoint to get all todos
+def calculate_combinations(all_stickers, word):
+  combinations = [get_stickers_starting_with(all_stickers, word)]
+  return combinations
+
+def get_stickers_starting_with(all_stickers, starting_with):
+  return list(filter(lambda s: s['letters'].lower().startswith(starting_with), all_stickers))
+
 @app.route('/suggest/<word>', methods=['GET'])
-def get_todos(word):
+def get_suggestion(word):
   print(f'GET /suggest/{word}')
-  return [list(filter(lambda s: word in s['letters'].lower(), data))[:5]]
+  return calculate_combinations(all_stickers, word)
 
 if __name__ == '__main__':
     app.run(debug=True)
