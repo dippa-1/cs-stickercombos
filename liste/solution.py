7fa3324c7a2a546f230312ad5b4ed346c1a5dbf8
kilian <kilian.hammer@gmx.de>
2024-02-09 20:01:29 +0100

    add json files and python parser

---
+++ liste/solution.py
@@ -0,0 +1,18 @@
+import json
+
+with open('cs2allItems.json', 'r', encoding='UTF-8') as file:
+  jsonList = json.loads(file.read())
+  stickerList = []
+  
+  for item in jsonList:
+    stickerItem = {}
+    stickerItem['name'] = item['name']
+    stickerItem['iconUrl'] = item['image']
+    stickerItem['rarity'] = item['name'].split()[-1] if ('(' and ')') in item['name'].split()[-1] else 'Paper'
+    stickerItem['letters'] = [char for char in item['name'].split('| ')[1].split('(')[0] if not char == ' ']
+    stickerList.append(stickerItem)
+
+with open('stickerAll.json', 'w', encoding='UTF-8') as outFile:
+  json.dump(stickerList, outFile, indent=2)
+
+print(len(stickerList))
