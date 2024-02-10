c904def7b96cfa7beada75771c1751f7b4840b7c
dippa-1 <dominik.helfenstein@gmx.de>
2024-02-10 12:25:14 +0100

    server: make it thread safe

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_cors import CORS
 import json
+import threading
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,7 @@ with open('data/stickerAll.json', 'r') as f:
   all_stickers = json.load(f)
   
 sticker_iterator = iter(all_stickers)
+sticker_semaphore = threading.Semaphore()
 
 def calculate_combinations(all_stickers, word):
   combinations = [get_stickers_starting_with(all_stickers, word)]
@@ -28,10 +30,14 @@ def get_suggestion(word):
 @app.route('/label/next', methods=['GET'])
 def get_next_label():
   print('GET /label/next')
+  sticker_semaphore.acquire()
   try:
-    return next(sticker_iterator)
+    sticker = next(sticker_iterator)
+    sticker_semaphore.release()
+    return sticker
   except StopIteration:
-    return 'No more stickers'
+    sticker_semaphore.release()
+    return 'No more stickers', 404
 
 if __name__ == '__main__':
     app.run(debug=True)
