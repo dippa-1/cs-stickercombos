1c52b60fa0d29f60bc0e74bc8ff7278aa83491bf
kilian <kilian.hammer@gmx.de>
2024-02-10 15:01:08 +0100

    corrected algorithm

--- server/algo.py
+++ server/algo.py
@@ -1,18 +1,15 @@
 import json
 
-
 def find_combinations(word, stickerList, stickerId=[], depth=0):
     if word == '':
         return stickerId
     
     if depth == 5:
-        return
+        return None
     
     for sticker in stickerList:
-        # Extrahieren Sie die Buchstaben aus jedem Sticker-Text
         words = [x['letters'] for x in sticker['text']]
 
-        # Durchlaufen Sie die extrahierten Buchstaben für jeden Sticker
         for text in words:
             text = text.lower().replace(' ', '')
 
@@ -20,13 +17,17 @@ def find_combinations(word, stickerList, stickerId=[], depth=0):
                 if word.startswith(text[:i]):
                     new_word = word.replace(text[:i], '', 1)
                     stickerId.append(sticker)
-                    find_combinations(new_word, stickerList, stickerId, depth + 1)
-        
+                    result = find_combinations(new_word, stickerList, stickerId, depth + 1)
+                    if result is not None:
+                        return result
+                    else:
+                        stickerId.pop()
+    return None
 
 # Beispielaufruf
 with open('data/stickerAll2.json', 'r', encoding='UTF-8') as f:
-  stickerList = json.loads(f.read())
-  word = "big dick"
-  stickerId = []
-  result = [find_combinations(word.lower().replace(' ', ''), stickerList)]
-  print(result)
+    stickerList = json.loads(f.read())
+    word = "big dick"
+    stickerId = []
+    result = find_combinations(word.lower().replace(' ', ''), stickerList)
+    print(result)
