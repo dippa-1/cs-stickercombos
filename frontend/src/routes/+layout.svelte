836b1efa9678293d42ea0b47b2cc20e90b79ba02
dippa-1 <dominik.helfenstein@gmx.de>
2024-02-10 12:12:20 +0100

    web: add github button

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
 	import '../app.pcss';
+
+	import GitHubButton from '$lib/GitHubButton.svelte';
 </script>
+
+<div class="relative">
+	<div class="absolute right-2 top-2">
+		<GitHubButton />
+	</div>
+</div>
 
 <div class="mx-auto p-8 text-center md:p-16">
 	<slot />
