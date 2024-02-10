61baff38f2938f6ccb7f387293828fc3590fd31e
kilian <kilian.hammer@gmx.de>
2024-02-10 09:43:28 +0100

    added more information to json

--- server/data/update_data.py
+++ server/data/update_data.py
@@ -1,5 +1,25 @@
 import json
 
+def get_trait(name: str) -> str:
+  if '(Glitter)' in name:
+    return 'Glitter'
+  elif '(Foil)' in name:
+    return 'Foil'
+  elif '(Holo)' in name:
+    return 'Holo'
+  elif '(Gold)' in name:
+    return 'Gold'
+  elif '(Lenticular)' in name:
+    return 'Lenticular'
+  else:
+    return 'Paper'
+  
+def get_rarity(name: str) -> str:
+  if name == 'Default' or name == 'High Grade':
+    return 'High Grade'
+  else:
+    return name
+
 with open('cs2allItems.json', 'r', encoding='UTF-8') as file:
   jsonList = json.loads(file.read())
   stickerList = []
@@ -8,11 +28,18 @@ with open('cs2allItems.json', 'r', encoding='UTF-8') as file:
     stickerItem = {}
     stickerItem['name'] = item['name']
     stickerItem['iconUrl'] = item['image']
-    stickerItem['rarity'] = item['name'].split()[-1] if ('(' and ')') in item['name'].split()[-1] else 'Paper'
-    stickerItem['letters'] = item['name'].split('| ')[1].split('(')[0].replace(' ', '')
+    stickerItem['trait'] = get_trait(item['name'])
+
+    if 'rarity' in item:
+      stickerItem['rarity'] = get_rarity(item['rarity']['name'])
+      stickerItem['rarity_color'] = item['rarity']['color']
+
+    stickerItem['text'] = []
+    stickerItem['text'].append(dict(letters = item['name'].split('| ')[1].split('(')[0].replace(' ', ''), rotation = 0))
+
     stickerList.append(stickerItem)
 
-with open('stickerAll.json', 'w', encoding='UTF-8') as outFile:
+with open('stickerAll2.json', 'w', encoding='UTF-8') as outFile:
   json.dump(stickerList, outFile, indent=2)
 
 print(len(stickerList))
