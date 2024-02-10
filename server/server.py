e154a97741e1ebb349d14edec84281d0b7f85884
dippa-1 <dominik.helfenstein@gmx.de>
2024-02-10 12:12:51 +0100

    add simple labeling endpoint and ui

--- server/server.py
+++ server/server.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 all_stickers = None
 with open('data/stickerAll.json', 'r') as f:
   all_stickers = json.load(f)
+  
+sticker_iterator = iter(all_stickers)
 
 def calculate_combinations(all_stickers, word):
   combinations = [get_stickers_starting_with(all_stickers, word)]
@@ -20,6 +24,14 @@ def get_stickers_starting_with(all_stickers, starting_with):
 def get_suggestion(word):
   print(f'GET /suggest/{word}')
   return calculate_combinations(all_stickers, word)
+
+@app.route('/label/next', methods=['GET'])
+def get_next_label():
+  print('GET /label/next')
+  try:
+    return next(sticker_iterator)
+  except StopIteration:
+    return 'No more stickers'
 
 if __name__ == '__main__':
     app.run(debug=True)
