c6639a943f7fb1b789d50850dc0433f61a45efbd
Dominik Helfenstein <dominik.helfenstein@gmx.de>
2024-02-11 14:46:02 +0100

    web: fix labeling

--- frontend/src/routes/label/+page.server.ts
+++ frontend/src/routes/label/+page.server.ts
@@ -1,7 +1,7 @@
 import { error, redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load: PageLoad = async ({}) => {
+export const load: PageServerLoad = async () => {
   const sticker: {
     name: string,
     iconUrl: string,
